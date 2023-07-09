a5a356c4a025fa9bd1093c1e2b60687257785ca0
Harmeet Singh Gorowada <harmeetsingh7276@gmail.com>
2023-07-09 19:50:17 +0530

    Create,delete,viewall tasks completed

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export const TodoForm = () => {
+export const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (value) {
+      console.log(value);
+      addTodo(value);
+      setValue('');
+    }
+  };
+
   return (
-        <form className='TodoForm'>
-            <input type='text' className='todo-input' placeholder='What is the task today?'/>
-            <button type='submit' className='todo-btn'></button>
-        </form>
-  )
-}
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        placeholder="What is the task today?"
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+      />
+      <button type="submit" className="todo-btn">
+        Add Task
+      </button>
+    </form>
+  );
+};
